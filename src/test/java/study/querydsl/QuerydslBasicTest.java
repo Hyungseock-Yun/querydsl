4fe646cb63bcb342c6eaa0c61d7ca1a95cf90fea
hsyoon <bagzein@gmail.com>
2022-04-21 02:29:26 +0900

    동적쿼리 - BooleanBuilder 사용 - Where 다중 파라미터 사용
    - 수정, 삭제 벌크 연산
    - SQL Function 호출

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,8 +1,11 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -568,6 +571,143 @@ public class QuerydslBasicTest {
 
     for (MemberDto memberDto : result) {
       System.out.println("memberDto = " + memberDto);
+    }
+  }
+
+  @Test
+  public void dynamicQuery_BooleanBuilder() {
+    String usernameParam = "member1";
+    Integer ageParam = null;
+
+    List<Member> result = searchMember1(usernameParam, ageParam);
+    assertThat(result.size()).isEqualTo(1);
+  }
+
+  private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+    BooleanBuilder builder = new BooleanBuilder();
+//    BooleanBuilder builder = new BooleanBuilder(member.username.eq(usernameCond));    // 필수 조건으로 만들수도 있음
+    if (usernameCond != null) {
+      builder.and(member.username.eq(usernameCond));
+    }
+
+    if (ageCond != null) {
+      builder.and(member.age.eq(ageCond));
+    }
+
+    return queryFactory
+      .selectFrom(member)
+      .where(builder)
+      .fetch();
+  }
+
+  @Test
+  public void dynamicQuery_WhereParam() {
+    String usernameParam = "member1";
+    Integer ageParam = 10;
+
+    List<Member> result = searchMember2(usernameParam, ageParam);
+    assertThat(result.size()).isEqualTo(1);
+  }
+
+  private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+    return queryFactory
+      .selectFrom(member)
+//      .where(usernameEq(usernameCond), ageEq(ageCond))
+      .where(allEq(usernameCond, ageCond))
+      .fetch();
+  }
+
+  private BooleanExpression usernameEq(String usernameCond) {
+
+    return usernameCond == null ? null : member.username.eq(usernameCond);
+  }
+
+  private BooleanExpression ageEq(Integer ageCond) {
+
+    return ageCond == null ? null : member.age.eq(ageCond);
+  }
+
+  private BooleanExpression allEq(String usernameCond, Integer ageCond) {   // null 처리 필요
+    return usernameEq(usernameCond).and(ageEq(ageCond));
+  }
+
+  @Test
+  public void bulkUpdate() {
+
+    // member1 = 10 -> DB member1
+    // member2 = 20 -> DB member2
+    // member3 = 30 -> DB member3
+    // member4 = 40 -> DB member4
+
+    long count = queryFactory
+      .update(member)
+      .set(member.username, "비회원")
+      .where(member.age.lt(28))
+      .execute();
+
+    // 영속성 컨텍스트 때문에 초기화.
+    em.flush();
+    em.clear();
+
+    // member1 = 10 -> DB 비회원
+    // member2 = 20 -> DB 비회원
+    // member3 = 30 -> DB member3
+    // member4 = 40 -> DB member4
+
+    System.out.println("count = " + count);
+
+    List<Member> result = queryFactory
+      .selectFrom(member)
+      .fetch();
+
+    for (Member member : result) {
+      System.out.println("member = " + member);
+    }
+  }
+
+  @Test
+  public void bulkAdd() {
+    long count = queryFactory
+      .update(member)
+      .set(member.age, member.age.multiply(2))
+      .execute();
+  }
+
+  @Test
+  public void bulkDelete() {
+    long count = queryFactory
+      .delete(member)
+      .where(member.age.gt(18))
+      .execute();
+  }
+
+  @Test
+  public void sqlFunction() {
+    List<String> result = queryFactory
+      .select(Expressions.stringTemplate(
+        "function('replace', {0}, {1}, {2})",
+        member.username, "member", "M"))
+      .from(member)
+      .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void sqlFunction2() {
+    List<String> result = queryFactory
+      .select(member.username)
+      .from(member)
+//      .where(member.username.eq(
+//        Expressions.stringTemplate("function('lower', {0})", member.username))
+//      )
+      .where(member.username.eq(member.username.lower()))
+      .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
     }
   }
 }
