9093c40bbcacb34d4c485bc9eb0ed38fa1b881e3
hsyoon <bagzein@gmail.com>
2022-04-23 16:45:28 +0900

    - Spring Data JPA Repository로 변경 - 사용자 정의 Repository - Spring Data 페이징 활용(Querydsl 연동) - Spring Data 페이징 활용(Count 쿼리 최적화)

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,5 @@
 package study.querydsl.repository;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +12,7 @@ import study.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
