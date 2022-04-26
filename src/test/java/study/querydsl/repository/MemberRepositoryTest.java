9887b1daff2f82255e13edd3f9ed164f4bd19372
hsyoon <bagzein@gmail.com>
2022-04-27 02:53:45 +0900

    - Spring Data 페이징 활용(Controller 개발) - QuerydslPredicateExecutor (실무에서 사용에는 한계가 존재)

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,12 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -115,5 +117,24 @@ public class MemberRepositoryTest {
     Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
     assertThat(result.getSize()).isEqualTo(3);
     assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+  }
+
+  @Test
+  public void querydslPredicateExecutorTest() {
+    Member member1 = new Member("member1", 10);
+    Member member2 = new Member("member2", 20);
+
+    Member member3 = new Member("member3", 30);
+    Member member4 = new Member("member4", 40);
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+
+    QMember member = QMember.member;
+    Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+    for (Member findMember : result) {
+      System.out.println("member = " + findMember);
+    }
   }
 }
